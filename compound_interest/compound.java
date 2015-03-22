928eb94a16ad06dd8ec534ba8878e5bc463664ce
Samantha Zeitlin <samanthazeitlin@gmail.com>
2015-03-22 16:46:03 -0700

    do git commit while class not yet finished

---
+++ compound_interest/compound.java
@@ -0,0 +1,15 @@
+class subscribers{
+	public static void main(String[] args) {
+		double amount;	
+		double principal = 10000;
+		double rate = 0.01;
+		
+		
+			
+		for(int day=1; day<=20; day++){
+			amount = principal*Math.pow(1+rate, day);
+			System.out.println(day + "  " + amount);
+		}
+			
+	}
+}
---
+++ do_while/do_while.java
@@ -0,0 +1,11 @@
+class cloud{
+	public static void main(String[] args){
+		int counter = 0;
+		
+		do{
+			System.out.println(counter);
+			counter++;
+			
+		}while(counter<=10);
+	}
+}
