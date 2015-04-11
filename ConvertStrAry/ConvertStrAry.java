0350017adffa9cb1562a284f3752672cb4df0f8e
Samantha Zeitlin <samanthazeitlin@gmail.com>
2015-04-10 19:27:33 -0700

    things like converting and printing arrays

--- ConvertStrAry/ConvertStrAry.java
+++ ConvertStrAry/ConvertStrAry.java
@@ -1,25 +1,45 @@
+import java.util.Arrays;
+
 public class ConvertStrAry {
 
-public static void main(String args[]) { String strVals[];
+	public static void main(String args[]) { 
 
-int intVals[];
+/* Add code here to instantiate an array of String that can hold 5 elements */ 
 
-/* Add code here to instantiate an array of String that can hold 5 elements */ /* Add code here to instantiate 5 String objects and add them to the array */ 
+String strVals[] = new String[5];
+int intVals[] = new int[5];
+
+/* Add code here to instantiate 5 String objects and add them to the array */ 
+strVals[0] = "1";
+strVals[1] = "2";
+strVals[2] = "37";
+strVals[3] = "42";
+strVals[4] = "77";
+
+
 intVals = convertToIntAry (strVals);
 
 /* Add code here to print the contents of the integer array that was returned */
 
+System.out.println(Arrays.toString(intVals));
+
 }
 
 /* Complete the function below – it returns an array of integers */
-public static int[] convertToIntAry(String strAry[]) {
+public static int[] convertToIntAry(String strVals[]) {
 
 // Instantiate an array of integers the same size as the String array 
 
+int intAry[] = new int[5];
+
 // Convert each String to an integer and store the result in the integer array 
-
-// return the integer array 
+for(int counter=0; counter<strVals.length; counter+=1){
+	int asInt = Integer.parseInt(strVals[counter]);
+	intAry[counter] = asInt;
 }
+// return the integer array 
+	return intAry;
 
+}
 }
 
